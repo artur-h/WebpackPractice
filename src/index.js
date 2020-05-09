aff75ba9c242830a110c4443d5d30939070c1e7a
artur-h <artur.hlushko@gmail.com>
2020-05-09 21:27:56 +0300

    Enabled dev-server & optimized use of libraries

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import Post from './Post.js';
+import * as $ from 'jquery';
+import Post from '@models/Post';
 import json from './assets/json.json';
 import xml from './assets/data.xml';
 import csv from './assets/data.csv';
-import WebpackLogo from './assets/webpack-logo.png';
+import WebpackLogo from '@/assets/webpack-logo.png';
 import './styles/styles.css';
 
 const post = new Post('Webpack Post title', WebpackLogo);
 
-console.log('Post toString:', post.toString())
+$('pre').addClass('code').html(post.toString());
 
 console.log('JSON', json);
 console.log('XML', xml);
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,21 @@ module.exports = {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
+  resolve: {
+    extensions: ['.js', '.json', '.png'],
+    alias: {
+      '@models': path.resolve(__dirname, 'src/models'),
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  devServer: {
+    port: 4200
+  },
   plugins: [
     new HTMLWebpackPlugin({
       template: './index.html'
