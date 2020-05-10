dda62f6d594e04a25e570152a8d01e78a6197452
artur-h <artur.hlushko@gmail.com>
2020-05-10 12:26:57 +0300

    Added eslint & react and other plugins to webpack

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,34 @@ import json from './assets/json.json';
 import xml from './assets/data.xml';
 import csv from './assets/data.csv';
 import WebpackLogo from '@/assets/webpack-logo.png';
+import React from 'react';
+import {render} from 'react-dom';
 import './babel';
 import './styles/styles.css';
 import './styles/less.less';
 import './styles/scss.scss';
 
 const post = new Post('Webpack Post title', WebpackLogo);
-
 $('pre').addClass('code').html(post.toString());
+
+const App = () => (
+  <div className="container">
+    <h1>Webpack Practice</h1>
+    <hr />
+    <div className="logo" />
+    <hr />
+    <pre />
+    <hr />
+    <div className="box">
+      <h2>Less</h2>
+    </div>
+    <div className="card">
+      <h2>Scss</h2>
+    </div>
+  </div>
+);
+
+render(<App/>, document.getElementById('app'));
 
 console.log('JSON', json);
 console.log('XML', xml);
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
@@ -47,12 +48,69 @@ const cssLoaders = extra => {
   return loaders;
 };
 
+const babelOptions = preset => {
+  const opts = {
+    presets: [
+      '@babel/preset-env'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties'
+    ]
+  }
+
+  if (preset) {
+    opts.presets.push(preset);
+  }
+
+  return opts;
+};
+
+const jsLoaders = () => {
+  const loaders = [{
+    loader: 'babel-loader',
+    options: babelOptions()
+  }];
+
+  if (isDev) {
+    loaders.push('eslint-loader');
+  }
+
+  return loaders;
+};
+
+const plugins = () => {
+  const base = [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist')
+      }
+    ]),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ]
+
+  if (isProd) {
+    base.push(new BundleAnalyzerPlugin());
+  }
+
+  return base;
+};
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: {
-    main: ['@babel/polyfill', './index.js'],
-    analytics: './analytics.js'
+    main: ['@babel/polyfill', './index.jsx'],
+    analytics: './analytics.ts'
   },
   output: {
     filename: filename('js'),
@@ -70,24 +128,8 @@ module.exports = {
     port: 4200,
     hot: isDev
   },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: './index.html',
-      minify: {
-        collapseWhitespace: isProd
-      }
-    }),
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, 'src/favicon.ico'),
-        to: path.resolve(__dirname, 'dist')
-      }
-    ]),
-    new MiniCssExtractPlugin({
-      filename: filename('css')
-    })
-  ],
+  devtool: isDev ? 'source-map': '',
+  plugins: plugins(),
   module: {
     rules: [
       {
@@ -121,16 +163,22 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
+        use: jsLoaders()
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
         loader: {
           loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env'
-            ],
-            plugins: [
-              '@babel/plugin-proposal-class-properties'
-            ]
-          },
+          options: babelOptions('@babel/preset-typescript')
+        }
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: 'babel-loader',
+          options: babelOptions('@babel/preset-react')
         }
       }
     ]
